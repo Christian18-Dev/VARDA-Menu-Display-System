094fa57ae4fc5f9407dea2a6692d840b882d1ad7
christianpaulrioja18@gmail.com <christianpaulrioja18@gmail.com>
2025-07-01 08:17:10 +0800

    gh-pages setup #2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/VARDA-Menu-Display-System',
+  base: '/VARDA-Menu-Display-System/',
   plugins: [react()],
   server: {
     port: 3001,
